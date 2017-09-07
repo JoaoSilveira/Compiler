adcf018ff6b2dc2ea076cd95d33cb09ab0bb1dd2
João Pedro Silveira <joao.pedro0842@hotmail.com>
2017-09-07 18:06:25 -0300

    Start Lexical Analyzer

---
+++ Compiler/LexicalAnalyzer.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Compilador
+{
+    class LexicalAnalyzer
+    {
+        private StreamReader Reader { get; }
+
+        private StringBuilder Builder { get; }
+
+        private char GetChar => (char)Reader.Peek();
+
+        private Func<bool> State { get; set; }
+
+        public LexicalAnalyzer(StreamReader reader)
+        {
+            Reader = reader;
+            Builder = new StringBuilder();
+            State = State1;
+        }
+
+        public IEnumerable<Token> GetTokens()
+        {
+            while (!Reader.EndOfStream)
+            {
+                if (State())
+                    yield return GetToken();
+
+                Next();
+            }
+        }
+
+        private bool State1()
+        {
+            var c = GetChar;
+            Push(c);
+
+            if (IsInitOfId(c)) // leu um caracter
+            {
+                State = State2;
+
+                return false;
+            }
+
+            if (c == '-')
+            {
+                State = State3;
+
+                return false;
+            }
+
+            if (char.IsDigit(c))
+            {
+                State = State4;
+
+                return false;
+            }
+
+            if (c == '.')
+            {
+                State = State5;
+
+                return false;
+            }
+
+            throw new Exception("Caracter não reconhecido");
+        }
+
+        private bool State2()
+        {
+            var c = GetChar;
+
+            if (!IsBodyOfId(c))
+            {
+                return true;
+            }
+
+            Push(c);
+            return false;
+        }
+
+        private bool State3()
+        {
+            var c = GetChar;
+
+            if (char.IsNumber(c))
+            {
+                Push(c);
+                State = State4;
+                return false;
+            }
+
+            if (c != '.')
+                return true;
+
+            Push(c);
+            State = State5;
+            return false;
+        }
+
+        private bool State4()
+        {
+            var c = GetChar;
+
+            if (char.IsNumber(c))
+            {
+                Push(c);
+                return false;
+            }
+
+            if (c != '.')
+                return true;
+
+            Push(c);
+            State = State5;
+            return false;
+        }
+
+        private bool State5()
+        {
+            var c = GetChar;
+
+            if (!char.IsDigit(c))
+                throw new Exception("Espera-se pelo menos um dígito após o ponto");
+
+            Push(c);
+            State = State6;
+            return false;
+        }
+
+        private bool State6()
+        {
+            var c = GetChar;
+
+            if (!char.IsNumber(c))
+                return true;
+
+            Push(c);
+            return false;
+        }
+
+        private void Next()
+        {
+            Reader.BaseStream.Position += 1;
+        }
+
+        private void Back()
+        {
+            Reader.BaseStream.Position -= 1;
+        }
+
+        private void Push(char c)
+        {
+            Builder.Append(c);
+        }
+
+        private Token GetToken()
+        {
+            var tk = new Token(Builder.ToString());
+            Builder.Clear();
+            Back();
+
+            return tk;
+        }
+
+        public static bool IsInitOfId(char c)
+        {
+            return char.IsLetter(c) || c == '_';
+        }
+
+        public static bool IsBodyOfId(char c)
+        {
+            return char.IsLetterOrDigit(c) || c == '_';
+        }
+    }
+}
---
+++ Compiler/Token.cs
@@ -0,0 +1,40 @@
+﻿using System.CodeDom;
+using System.Text.RegularExpressions;
+
+namespace Compilador
+{
+    public class Token
+    {
+        public string Text { get; }
+
+        public Token(string text)
+        {
+            Text = text;
+        }
+
+        public static Token GetFrom(string content)
+        {
+            if (Regex.IsMatch(content, "^[_a-zA-Z][_a-zA-Z0-9]*$"))
+            {
+                // é um ID - tem que verificar se é palavra reservada
+            }
+
+            if (Regex.IsMatch(content, @"^-?(\d*\.\d+|\d+)$"))
+            {
+                // é um número - tem que ver se é decimal ou não
+            }
+
+            if (Regex.IsMatch(content, "^\"[^\n\"]*\"$"))
+            {
+                // é uma string
+            }
+
+            if (Regex.IsMatch(content, @"^(\+|\*|%|&|\||\#|\{|\}|\(|\)|;|==|<=|>=|!=|=|<|>|!|/|-)$"))
+            {
+                // é um operador ou caracter de controle tem que verificar
+            }
+
+            return null;
+        }
+    }
+}
